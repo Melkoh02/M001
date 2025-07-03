3760fce8cf2d67d09e9b1c809ed6f33f739cd233
Melkoh <git@melkoh.dev>
2025-07-03 04:51:14 -0300

    chore(api): remove test endpoints and test API call
    - Remove all test endpoints from 'endpoints.ts'
    - Remove test API call from `HomeScreen.tsx`

--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -2,29 +2,10 @@ import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {useTheme} from '../lib/hooks/useAppTheme.ts';
 import {useTranslation} from 'react-i18next';
-import {Button} from 'react-native-paper';
-import useApi from '../lib/hooks/useApi.ts';
 
 export default function HomeScreen() {
   const theme = useTheme();
   const {t} = useTranslation();
-  const api = useApi();
-  const [loading, setLoading] = React.useState(true);
-
-  const testApiCalls = () => {
-    setLoading(true);
-    api.getTest().handle({
-      onSuccess: payload => {
-        console.log('Got back:', payload.test);
-      },
-      onError: err => {
-        console.log('Server replied with an error:', err.response?.status);
-      },
-      successMessage: 'test endpoint succeeded!',
-      errorMessage: 'test endpoint failed!',
-      onFinally: () => setLoading(false),
-    });
-  };
 
   return (
     <View
@@ -35,12 +16,6 @@ export default function HomeScreen() {
       <Text style={{color: theme.colors.primary, padding: 10}}>
         {t('home.title')}
       </Text>
-      <Button
-        mode="contained-tonal"
-        onPress={testApiCalls}
-        style={styles.button}>
-        Test API Call
-      </Button>
     </View>
   );
 }
