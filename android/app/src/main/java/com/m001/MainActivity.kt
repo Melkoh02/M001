3b136aacd37baf6610a48960aa79bd133cc9f29a
Melkoh <git@melkoh.dev>
2025-07-10 04:25:42 -0300

    feat(splash-screen): add custom splash screen

--- android/app/src/main/java/com/m001/MainActivity.kt
+++ android/app/src/main/java/com/m001/MainActivity.kt
@@ -1,22 +1,26 @@
 package com.m001
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import org.devio.rn.splashscreen.SplashScreen
 
 class MainActivity : ReactActivity() {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
+  override fun onCreate(savedInstanceState: Bundle?) {
+    // show the native splash before React content is initialized
+    SplashScreen.show(this)
+    super.onCreate(savedInstanceState)
+  }
+
+  /** Must match your app name in index.js */
   override fun getMainComponentName(): String = "M001"
 
   /**
-   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   * Use the default delegate (with Fabric toggle)
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+    DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
 }
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,9 +1,17 @@
-import {observer} from 'mobx-react-lite';
+import {useEffect} from 'react';
 
+import {observer} from 'mobx-react-lite';
 import BaseLayout from '../components/templates/BaseLayout';
 import Routes from '../routes/routes';
+import SplashScreen from 'react-native-splash-screen';
+import {Platform} from 'react-native';
 
 export default observer(function App() {
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      SplashScreen.hide();
+    }
+  }, []);
   return (
     <BaseLayout>
       <Routes />
