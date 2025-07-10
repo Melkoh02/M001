3da3d929ff490b9fe7240dbd0e718240a6db5e9c
Melkoh <git@melkoh.dev>
2025-07-10 06:00:25 -0300

    feat(splash-screen): wait for `isHydrated` flag to hide splash screen

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,17 +1,21 @@
 import {useEffect} from 'react';
-
+import {Platform} from 'react-native';
 import {observer} from 'mobx-react-lite';
+import SplashScreen from 'react-native-splash-screen';
+
 import BaseLayout from '../components/templates/BaseLayout';
 import Routes from '../routes/routes';
-import SplashScreen from 'react-native-splash-screen';
-import {Platform} from 'react-native';
+import {useStore} from '../lib/hooks/useStore.ts';
 
 export default observer(function App() {
+  const {userStore} = useStore();
+
   useEffect(() => {
     if (Platform.OS === 'android') {
       SplashScreen.hide();
     }
-  }, []);
+  }, [userStore.isHydrated]);
+
   return (
     <BaseLayout>
       <Routes />
