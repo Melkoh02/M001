5126b5193c462a1ff7d69687647466a06db91f3e
Melkoh <melkoh.dev@gmail.com>
2025-11-23 11:43:17 -0300

    feat: use BaseLayout on settings screen

--- src/pages/SettingsScreen.tsx
+++ src/pages/SettingsScreen.tsx
@@ -1,26 +1,24 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text} from 'react-native';
 import {useTranslation} from 'react-i18next';
 import {observer} from 'mobx-react-lite';
 import {useTheme} from '../lib/hooks/useAppTheme';
+import BaseLayout from '../components/templates/BaseLayout.tsx';
 
 const SettingsScreen = () => {
   const theme = useTheme();
   const {t} = useTranslation();
 
   return (
-    <View
-      style={[styles.container, {backgroundColor: theme.colors.background}]}>
+    <BaseLayout>
       <Text style={[styles.title, {color: theme.colors.primary}]}>
         {t('settings.title')}
       </Text>
-    </View>
+    </BaseLayout>
   );
 };
 
 export default observer(SettingsScreen);
 
 const styles = StyleSheet.create({
-  container: {flex: 1, justifyContent: 'center', alignItems: 'center'},
   title: {padding: 10},
-  button: {marginTop: 16},
 });
